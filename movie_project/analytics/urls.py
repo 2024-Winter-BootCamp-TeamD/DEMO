f05d5017d038f69c925687199e78494514f947c7
fostacion <78186157+fostacion@users.noreply.github.com>
2025-01-26 23:54:21 +0900

    Revert "영화리뷰프로젝트 예시 코드"

--- movie_project/analytics/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import getAllAnalytics, AggregatedDataView
-
-urlpatterns = [
-    # (Clean) 함수 이름이 모호 (getAllAnalytics)
-    path('all/', getAllAnalytics, name='all_analytics'),
-    path('aggregated/', AggregatedDataView.as_view(), name='aggregated_data'),
-]
--- movie_project/search/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import searchAll, FilterSearchView
-
-urlpatterns = [
-    # (Clean) 함수/클래스 이름이 직관적이지 않음
-    path('all/', searchAll, name='search_all'),
-    path('filter/', FilterSearchView.as_view(), name='filter_search'),
-]
