3d12a89118164ba1c2a3dfcd999e57ced91ece44
RYUJEONGHUN <ryugh0210@naver.com>
2025-01-30 02:50:01 +0900

    삭제

--- cleantestB.py
+++
@@ -1,9 +0,0 @@
-def handle_input(text, number):
-    result = []
-    for i in range(number):
-        result.append(text + str(i))
-    print("Result:", result)
-
-    if len(result) > 10:
-        print("Too many results!")
-    return result
